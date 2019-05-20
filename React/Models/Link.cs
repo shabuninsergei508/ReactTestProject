dc42d0c169706ce0093ec2c32b5225abe46e48ee
shabuninsergei508 <shabuninsergei508@github.com>
2019-05-20 15:27:02 +0400

    модель с новыми полями

--- React/Models/Link.cs
+++ React/Models/Link.cs
@@ -10,10 +10,12 @@ namespace React.Models
 {
     public class Link
     {
-        public Link(string longLink, string shortLink)
+        public Link(string longLink, string shortLink, string tag, int discount)
         {
             LongLink = longLink;
             ShortLink = shortLink;
+            Tag = tag;
+            Discount = discount;
         }
 
         [Key]
@@ -25,5 +27,29 @@ namespace React.Models
 
         [Required]
         public string LongLink { get; set; }
+
+        [Required]
+        public string Tag { get; set; }
+
+        [Required]
+        public int Discount { get; set; }
+
+        //old model
+        //public Link(string longLink, string shortLink)
+        //{
+        //    LongLink = longLink;
+        //    ShortLink = shortLink;
+
+        //}
+
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public int Id { get; set; }
+
+        //[Required]
+        //public string ShortLink { get; set; }
+
+        //[Required]
+        //public string LongLink { get; set; }
     }
 }
